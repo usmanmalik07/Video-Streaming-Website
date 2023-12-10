a277a7a1e0dbd7114dace73969e1c33af317cbd2
Usman Malik <usmanmalikk2004@gmail.com>
2023-12-10 14:17:23 +0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+document.getElementById('imageContainer1').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/op.html";
+});
+document.getElementById('imageContainer2').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/tokyoghoul.html";
+});
+document.getElementById('imageContainer3').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/aot.html";
+});
+document.getElementById('imageContainer4').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/haikyuu.html";
+});
+document.getElementById('imageContainer5').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/charlotte.html";
+});
+document.getElementById('imageContainer6').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/hxh.html";
+});
+document.getElementById('imageContainer7').addEventListener('click', function() {
+  // Change the URL to the desired website
+  window.location.href = "watch/naruto.html";
+});
