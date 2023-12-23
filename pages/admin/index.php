9ea8c2e6d8f0782ae177530d2d27898b8111e78e
Usman Malik <usmanmalikk2004@gmail.com>
2023-12-23 20:15:46 +0500

    commit 23/12/23

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -104,7 +104,7 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-body">Stock Details</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
