97b187c937f691199c939838c12e57ace82d7993
Usman Malik <usmanmalikk2004@gmail.com>
2023-12-24 18:07:49 +0500

    24/12/23

--- script/script.js
+++ script/script.js
@@ -26,3 +26,24 @@ document.getElementById('imageContainer7').addEventListener('click', function() 
   // Change the URL to the desired website
   window.location.href = "watch/naruto.html";
 });
+
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Let's Talk";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
